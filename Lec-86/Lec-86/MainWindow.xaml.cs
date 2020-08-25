14b47900dfc23bb92625d7202c1bd7545ef5e705
sataped <k.satapat@kkumail.com>
2020-08-25 15:21:14 +0700

    HW-86
    satapat

---
+++ Lec-86/Lec-86/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lec_86
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedList = new SortedList();
+        }
+
+        private void AddBtn(object sender, RoutedEventArgs e)
+        {
+            sortedList.Add(int.Parse(Keytxt.Text),Valuetxt.Text);
+
+            ICollection collection = sortedList.Keys;
+
+            string data = "";
+            foreach (int key in collection)
+            {
+                data = data + " " + "\n" + sortedList[key].ToString();
+
+            }
+            MessageBox.Show("Count = : " + collection.Count + data + " ");
+        }
+    }
+    
+}
